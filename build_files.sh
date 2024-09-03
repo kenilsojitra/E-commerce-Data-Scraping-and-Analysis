912b571492a95222c72dfd2acc9b291fb6053e4f
kenilsojitra17 <kenilsojitra17@gmail.com>
2024-09-03 14:28:34 +0530

    some changes

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 
 echo " BUILD START"
 python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear [ You, 1 second ago Uncommitted changes
+python3.9 manage.py collectstatic --noinput --clear 
 echo " BUILD END"
